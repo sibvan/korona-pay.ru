4d8f65fe2caa1a9bb790a13e20ba5f40b0afbc28
sibvan <sibvan.dev@gmail.com>
2025-06-19 10:06:02 +0400

    Избавился от класса
    Извлёк всё из класса, т. к. он тут избыточен

--- src/main.js
+++ src/main.js
@@ -1,72 +1,57 @@
-class ExchangeRateCalculator {
-  lastChangedCurrency = null;
+let lastChangedCurrency = null;
 
-  constructor() {
-    this.rubleInputEl = document.querySelector("[data-js-ruble]");
-    this.lariInputEl = document.querySelector("[data-js-lari]");
-    this.rateInputEl = document.querySelector("[data-js-rate]");
-    this.inputEls = document.querySelectorAll("input");
-    this.bindEvents();
+const rubleInputEl = document.querySelector("[data-js-ruble]");
+const lariInputEl = document.querySelector("[data-js-lari]");
+const rateInputEl = document.querySelector("[data-js-rate]");
+const inputEls = document.querySelectorAll("input");
+
+const replaceWrongValue = (value) => {
+  const formattedString = value.replace(/,/g, ".").replace(/[^\d.]/g, "");
+  const dotCount = (formattedString.match(/\./g) || []).length;
+
+  if (!formattedString.includes(".") || dotCount === 1) {
+    return formattedString;
+  } else {
+    const arr = formattedString.split(".");
+    return `${arr[0]}.${arr[1]}`;
   }
+};
 
-  replaceWrongValue(value) {
-    const stringWithoutCommasAndLetters = value
-      .replace(/,/g, ".")
-      .replace(/[^\d.]/g, "");
-    const dotCount = (stringWithoutCommasAndLetters.match(/\./g) || []).length;
+const canCalculate = () => {
+  if (
+    rateInputEl.value.length > 0 &&
+    (rubleInputEl.value.length > 0 || lariInputEl.value.length > 0)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-    if (!stringWithoutCommasAndLetters.includes(".") || dotCount === 1) {
-      return stringWithoutCommasAndLetters;
-    } else {
-      const arr = stringWithoutCommasAndLetters.split(".");
-      return `${arr[0]}.${arr[1]}`;
+const exchangeCurrency = () => {
+  const rub = +rubleInputEl.value;
+  const lari = +lariInputEl.value;
+  const rate = +rateInputEl.value;
+  const tax = 1;
+
+  if (lastChangedCurrency === rubleInputEl) {
+    lariInputEl.value = Math.floor(rub / (rate * (1 + tax / 100)));
+  }
+  if (lastChangedCurrency === lariInputEl) {
+    rubleInputEl.value = Math.ceil(lari * rate + (lari * rate * tax) / 100);
+  }
+};
+
+inputEls.forEach((input) => {
+  input.addEventListener("input", (inputEvent) => {
+    inputEvent.target.value = replaceWrongValue(inputEvent.target.value);
+
+    if (inputEvent.target !== rateInputEl) {
+      lastChangedCurrency = inputEvent.target;
     }
-  }
 
-  canCalculate() {
-    if (
-      this.rateInputEl.value.length > 0 &&
-      (this.rubleInputEl.value.length > 0 || this.lariInputEl.value.length > 0)
-    ) {
-      return true;
-    } else {
-      return false;
+    if (canCalculate()) {
+      exchangeCurrency();
     }
-  }
-
-  exchangeCurrency() {
-    const rub = +this.rubleInputEl.value;
-    const lari = +this.lariInputEl.value;
-    const rate = +this.rateInputEl.value;
-    const tax = 1;
-
-    if (this.lastChangedCurrency === this.rubleInputEl) {
-      this.lariInputEl.value = Math.floor(rub / (rate * (1 + tax / 100)));
-    }
-    if (this.lastChangedCurrency === this.lariInputEl) {
-      this.rubleInputEl.value = Math.ceil(
-        lari * rate + (lari * rate * tax) / 100
-      );
-    }
-  }
-
-  bindEvents() {
-    this.inputEls.forEach((input) => {
-      input.addEventListener("input", (inputEvent) => {
-        inputEvent.target.value = this.replaceWrongValue(
-          inputEvent.target.value
-        );
-
-        if (inputEvent.target !== this.rateInputEl) {
-          this.lastChangedCurrency = inputEvent.target;
-        }
-
-        if (this.canCalculate()) {
-          this.exchangeCurrency();
-        }
-      });
-    });
-  }
-}
-
-new ExchangeRateCalculator();
+  });
+});
